a01ac1c4fe2a1e3096f10b5afbda1818158676be
Jen Poole <contact.vandepool@gmail.com>
2018-04-22 00:03:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+
+// is declared with const & is assigned an initial value of ["Milo", "Otis", "Garfield"]
+const drivers = ["Milo", "Otis", "Garfield"];
+
+// appends a driver to the end of the drivers array
+function destructivelyAppendDriver (name) {
+  drivers.push(name);
+}
+
+// prepends a driver to the beginning of the drivers array
+function destructivelyPrependDriver (name) {
+  drivers.unshift(name);
+}
+
+// removes the last driver from the drivers array
+function destructivelyRemoveLastDriver () {
+  drivers.pop();
+}
+
+// removes the First driver from the drivers array
+function destructivelyRemoveFirstDriver () {
+  drivers.shift();
+}
+
+// appends a driver to the drivers array and returns a new array, leaving the drivers array unchanged
+function appendDriver (name) {
+  return [...drivers, name];
+}
+
+// prepends a driver to the drivers array and returns a new array, leaving the drivers array unchanged
+function prependDriver (name) {
+  return [name, ...drivers];
+}
+
+// removes the first driver from the drivers array and returns a new array, leaving the drivers array unchanged
+function removeFirstDriver () {
+  return drivers.slice(1);
+}
+
+// removes the last driver in the drivers array and returns a new array, leaving the drivers array unchanged
+function removeLastDriver () {
+  return drivers.slice(0, drivers.length - 1);
+}
